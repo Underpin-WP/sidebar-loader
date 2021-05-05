b1d6d84a4aed540dba2dd6833df5940c257c4c75
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:53:48 -0700

    Corrects underpin autoloader

--- sidebars.php
+++ sidebars.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Sidebar.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Sidebar_Instance.php' );
-	$instance->loaders()->add( 'sidebars', [
+	Underpin\underpin()->get( $file )->loaders()->add( 'sidebars', [
 		'instance' => 'Underpin_Sidebars\Abstracts\Sidebar',
 		'default'  => 'Underpin_Sidebars\Factories\Sidebar_Instance',
 	] );
